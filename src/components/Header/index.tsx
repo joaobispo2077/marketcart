6e4a8b07767f1ee639cfb73f0876846d6883d714
João Bispo <joaobispo2077@gmail.com>
2021-11-02 17:23:19 -0300

    fix: render cart size into header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,7 +8,7 @@ import { useCart } from '../../hooks/useCart';
 
 const Header = (): JSX.Element => {
   const { cart } = useCart();
-  const cartSize = cart.reduce((acc, currentProduct) => acc + currentProduct.amount, 0);
+  const cartSize = cart.length;
 
   return (
     <Container>
